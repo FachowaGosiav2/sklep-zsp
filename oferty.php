33b773f7889265979aecff8e13dcb3c7ba1b60b4
Gracjan <97914318+FachowaGosiav2@users.noreply.github.com>
2022-11-09 22:19:24 +0100

    zmiana 4

--- oferty.php
+++ oferty.php
@@ -4,13 +4,20 @@
     <meta charset="UTF-8">
     <title>Document</title>
     <?php
-    $y = "SELECT ao.id, users_id, u.user_name, ao.product_name, ao.price, ao.content, ao.is_announcement FROM announcement_and_orders ao LEFT JOIN users u ON u.id = ao.users_id WHERE is_announcement = 1 AND users_id = ". $_SESSION['id'].";";
     session_start();
+    $x = new mysqli('127.0.0.1', 'root', '', 'mydb');
+    $y = $x->query("SELECT ao.id, users_id, u.user_name, ao.product_name, ao.price, ao.content, ao.is_announcement FROM announcement_and_orders ao LEFT JOIN users u ON u.id = ao.users_id WHERE is_announcement = 1 AND users_id = ". $_SESSION['id'].";");
+    $y2 = $y->fetch_all(MYSQLI_ASSOC);
     ?>
 </head>
 <body>
     <?php
-    echo $_SESSION['user_name'];
+    echo "twoje oferty<br>";
+    for($i = 0; $i < count($y2); $i++){
+        echo 'Numer ogłoszenia to '.$y2[$i]['id'].'<br>';
+        echo 'Cena produktu '.$y2[$i]['price'].'<br>';
+        echo 'Opisz<br>'.$y2[$i]['content'].'<br>';
+    }
     ?>
 </body>
 </html>
