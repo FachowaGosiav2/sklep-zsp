4c5f2d602b0fa3866c0edff8e79bfc50a348c0ad
Gracjan <97914318+FachowaGosiav2@users.noreply.github.com>
2022-11-09 21:39:06 +0100

    zakończone

---
+++ rejestracja.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <?php
+    $x = new mysqli('127.0.0.1', 'root', '', 'mydb');
+    ?>
+</head>
+<body>
+    <form method="POST">
+        Login:
+        <input type="text" name="user_name"><br>
+        Hasło:
+        <input type="password" name='password'><br>
+        Email
+        <input type="mail" name="mail"><br>
+        <button type="submit">Zarejestruje się</button>   
+    </form>
+
+    <?php
+    if(isset($_POST["user_name"]) and isset($_POST['password']) and isset($_POST["mail"])){
+        $y = "INSERT INTO `users`(`user_name`, `email`, `is_admin`, `password`) VALUES ('".$_POST["user_name"]."','".$_POST["mail"]."','0','".$_POST["password"]."')";
+        $x->query($y);
+        $x->close();
+        header('Location: logowanie.php');
+    }
+    ?>
+</body>
+</html>
