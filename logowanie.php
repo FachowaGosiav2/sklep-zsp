e18f29b2e4e74150c7f812a7f202033ebee414d7
FachowaGosiav2 <gracjan.broniecki@gmail.com>
2022-11-09 13:52:56 +0100

    zmiana 1

--- logowanie.php
+++ logowanie.php
@@ -6,11 +6,23 @@
     <?php
     $x = new mysqli('127.0.0.1', 'root', '', 'mydb');
     $y = $x->query('SELECT `user_name`, `password` FROM `users`;');
-    $y2 = $y->fetch_all(MYSQLI_ASSOC)
     ?>
 </head>
 <body>
-
+    <form method="$_POST">
+        Login:
+        <input type="text" name="login"><br>
+        Hasło:
+        <input type="password" name='password'><br>
+        <button type="submit">Zaloguj się</button>
+    </form>
+    <?php
+    if(isset($_POST('login')) and isset($_POST('password'))){
+        while($y2 = $y->fetch_all(MYSQLI_ASSOC)){
+            if()
+        }
+    }
+    ?>
     
 </body>
 </html>
